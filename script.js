fb0eec1a18f4d5c5ae04aec2d93bc53a5ae2aa44
Krishna Anand <83511292+Krishn-aa@users.noreply.github.com>
2025-02-10 20:04:32 +0530

    Update script.js
    Added new api key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TMDB
 
-const API_KEY = "api_key=f4abde90edc036d771807774ecbde7f4"; 
+const API_KEY = "api_key=5512dd976d383a74747ecd900d191d85"; 
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
